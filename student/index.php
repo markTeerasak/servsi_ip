9c78405b46af50faafd818411441ac85a910020d
markTeerasak <twichai4869@gmail.com>
2023-03-02 16:56:02 +0700

    change format to oop

--- student/index.php
+++ student/index.php
@@ -13,10 +13,11 @@ if(count($result)){
         'status' => true,
         'respoint' => $result
     );
-
+    http_response_code(200);
     echo json_encode($result, JSON_UNESCAPED_UNICODE);
 }else {
-
+    http_response_code(404);
+    echo json_encode(array('status' => false));
 }
 
 
--- student/logins.php
+++ student/logins.php
@@ -1,28 +1,25 @@
 <?php
 
 require_once "../connect.php";
+require_once "../response.php";
+
+$response = new Response();
 
 $params = array(
     'username' => $_GET['username'],
     'password' => $_GET['password']
 );
 
-$sql = "SELECT * FROM student WHERE student_id = :username AND password = :password";
+$sql = "SELECT student_id FROM student WHERE student_id = :username AND password = :password";
 $statement = $conn->prepare($sql);
 $statement->execute($params);
 
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 if(count($result)){
-    $response = array(
-        'status' => true,
-        'respoint' => $result
-    );
-    http_response_code(200);
-    echo json_encode($result, JSON_UNESCAPED_UNICODE);
+    $response->success($result, "success", 200);    
 }else {
-    http_response_code(404);
-    echo json_encode(array('status' => false));
+    $response->error('error not found!', 404);
 }
 
 
